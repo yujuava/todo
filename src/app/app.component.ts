f28c1c8714617f5e9a50f06171d80032a177b233
yujuava <yju031769@gmail.com>
2022-08-14 21:05:46 +0800

    update list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent {
+
+  
   title = 'todo';
 
   filter: 'all' | 'active' | 'done' = 'all';
@@ -18,11 +20,21 @@ export class AppComponent {
     { description: 'laugh', done: false },
   ];
 
+  
   get items() {
     if (this.filter === 'all') {
       return this.allItems;
     }
     return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
+  };
+
+  addItem(description: any) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
   }
+  
+  
 
 }
